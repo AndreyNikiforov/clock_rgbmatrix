0dc82f8280e2f98ebb4ffdc4b8af725facbba707
Andrey Nikiforov <andy.nikiforov@gmail.com>
2024-08-13 21:18:26 -0700

    clock without network

---
+++ README.md
@@ -0,0 +1,11 @@
+# download libs
+
+https://circuitpython.org/libraries
+
+# clock link
+
+https://learn.adafruit.com/network-connected-metro-rgb-matrix-clock/code-the-matrix-clock
+
+# problems
+
+adafruit-io lib is not available
--- circuitpy/code.py
+++ circuitpy/code.py
@@ -29,143 +29,128 @@ import framebufferio
 #)
 #display = framebufferio.FramebufferDisplay(matrix)
 
-# SPDX-FileCopyrightText: 2020 Jeff Epler for Adafruit Industries
+# SPDX-FileCopyrightText: 2020 John Park for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
 
-import random
-import time
+# Metro Matrix Clock
+# Runs on Airlift Metro M4 with 64x32 RGB Matrix display & shield
 
+import time
 import board
 import displayio
-import framebufferio
-import rgbmatrix
+import terminalio
+from adafruit_display_text.label import Label
+from adafruit_bitmap_font import bitmap_font
+# from adafruit_matrixportal.network import Network
+from adafruit_matrixportal.matrix import Matrix
 
+BLINK = True
+DEBUG = False
+
+# Get wifi details and more from a secrets.py file
+# try:
+#     from secrets import secrets
+# except ImportError:
+#     print("WiFi secrets are kept in secrets.py, please add them there!")
+#     raise
+print("    Metro Minimal Clock")
+# print("Time will be set for {}".format(secrets["timezone"]))
+
+# --- Display setup ---
 displayio.release_displays()
-
-# https://github.com/adafruit/Adafruit_Learning_System_Guides/tree/main/CircuitPython_RGBMatrix
-# Conway's "Game of Life" is played on a grid with simple rules, based
-# on the number of filled neighbors each cell has and whether the cell itself
-# is filled.
-#   * If the cell is filled, and 2 or 3 neighbors are filled, the cell stays
-#     filled
-#   * If the cell is empty, and exactly 3 neighbors are filled, a new cell
-#     becomes filled
-#   * Otherwise, the cell becomes or remains empty
-#
-# The complicated way that the "m1" (minus 1) and "p1" (plus one) offsets are
-# calculated is due to the way the grid "wraps around", with the left and right
-# sides being connected, as well as the top and bottom sides being connected.
-#
-# This function has been somewhat optimized, so that when it indexes the bitmap
-# a single number [x + width * y] is used instead of indexing with [x, y].
-# This makes the animation run faster with some loss of clarity. More
-# optimizations are probably possible.
-
-def apply_life_rule(old, new):
-    width = old.width
-    height = old.height
-    for y in range(height):
-        yyy = y * width
-        ym1 = ((y + height - 1) % height) * width
-        yp1 = ((y + 1) % height) * width
-        xm1 = width - 1
-        for x in range(width):
-            xp1 = (x + 1) % width
-            neighbors = (
-                old[xm1 + ym1] + old[xm1 + yyy] + old[xm1 + yp1] +
-                old[x   + ym1] +                  old[x   + yp1] +
-                old[xp1 + ym1] + old[xp1 + yyy] + old[xp1 + yp1])
-            new[x+yyy] = neighbors == 3 or (neighbors == 2 and old[x+yyy])
-            xm1 = x
-
-# Fill 'fraction' out of all the cells.
-def randomize(output, fraction=0.33):
-    for i in range(output.height * output.width):
-        output[i] = random.random() < fraction
-
-
-# Fill the grid with a tribute to John Conway
-def conway(output):
-    # based on xkcd's tribute to John Conway (1937-2020) https://xkcd.com/2293/
-    conway_data = [
-        b'  +++   ',
-        b'  + +   ',
-        b'  + +   ',
-        b'   +    ',
-        b'+ +++   ',
-        b' + + +  ',
-        b'   +  + ',
-        b'  + +   ',
-        b'  + +   ',
-    ]
-    for i in range(output.height * output.width):
-        output[i] = 0
-    for i, si in enumerate(conway_data):
-        y = output.height - len(conway_data) - 2 + i
-        for j, cj in enumerate(si):
-            output[(output.width - 8)//2 + j, y] = cj & 1
-
-# bit_depth=1 is used here because we only use primary colors, and it makes
-# the animation run a bit faster because RGBMatrix isn't taking over the CPU
-# as often.
 matrix = rgbmatrix.RGBMatrix(
-    width=64, bit_depth=4,
-    rgb_pins=[
-        board.MTX_R1,
-        board.MTX_G1,
-        board.MTX_B1,
-        board.MTX_R2,
-        board.MTX_G2,
-        board.MTX_B2
-    ],
-    addr_pins=[
-        board.MTX_ADDRA,
-        board.MTX_ADDRB,
-        board.MTX_ADDRC,
-        board.MTX_ADDRD
-    ],
-    clock_pin=board.MTX_CLK,
-    latch_pin=board.MTX_LAT,
-    output_enable_pin=board.MTX_OE
+   width=64, bit_depth=4,
+   rgb_pins=[
+       board.MTX_R1,
+       board.MTX_G1,
+       board.MTX_B1,
+       board.MTX_R2,
+       board.MTX_G2,
+       board.MTX_B2
+   ],
+   addr_pins=[
+       board.MTX_ADDRA,
+       board.MTX_ADDRB,
+       board.MTX_ADDRC,
+       board.MTX_ADDRD
+   ],
+   clock_pin=board.MTX_CLK,
+   latch_pin=board.MTX_LAT,
+   output_enable_pin=board.MTX_OE
 )
-display = framebufferio.FramebufferDisplay(matrix, auto_refresh=False)
-SCALE = 1
-b1 = displayio.Bitmap(display.width//SCALE, display.height//SCALE, 2)
-b2 = displayio.Bitmap(display.width//SCALE, display.height//SCALE, 2)
-palette = displayio.Palette(2)
-tg1 = displayio.TileGrid(b1, pixel_shader=palette)
-tg2 = displayio.TileGrid(b2, pixel_shader=palette)
-g1 = displayio.Group(scale=SCALE)
-g1.append(tg1)
-display.root_group = g1
-g2 = displayio.Group(scale=SCALE)
-g2.append(tg2)
+display = framebufferio.FramebufferDisplay(matrix)
+# network = Network(status_neopixel=board.NEOPIXEL, debug=False)
 
-# First time, show the Conway tribute
-palette[1] = 0xffffff
-conway(b1)
-display.auto_refresh = True
-time.sleep(3)
-n = 40
+# --- Drawing setup ---
+group = displayio.Group()  # Create a Group
+bitmap = displayio.Bitmap(64, 32, 2)  # Create a bitmap object,width, height, bit depth
+color = displayio.Palette(4)  # Create a color palette
+color[0] = 0x000000  # black background
+color[1] = 0xFF0000  # red
+color[2] = 0xCC4000  # amber
+color[3] = 0x85FF00  # greenish
+
+# Create a TileGrid using the Bitmap and Palette
+tile_grid = displayio.TileGrid(bitmap, pixel_shader=color)
+group.append(tile_grid)  # Add the TileGrid to the Group
+display.root_group = group
+
+if not DEBUG:
+    font = bitmap_font.load_font("/IBMPlexMono-Medium-24_jep.bdf")
+else:
+    font = terminalio.FONT
+
+clock_label = Label(font)
+
+
+def update_time(*, hours=None, minutes=None, show_colon=False):
+    now = time.localtime()  # Get the time values we need
+    if hours is None:
+        hours = now[3]
+    if hours >= 18 or hours < 6:  # evening hours to morning
+        clock_label.color = color[1]
+    else:
+        clock_label.color = color[3]  # daylight hours
+    if hours > 12:  # Handle times later than 12:59
+        hours -= 12
+    elif not hours:  # Handle times between 0:00 and 0:59
+        hours = 12
+
+    if minutes is None:
+        minutes = now[4]
+
+    if BLINK:
+        colon = ":" if show_colon or now[5] % 2 else " "
+    else:
+        colon = ":"
+
+    clock_label.text = "{hours}{colon}{minutes:02d}".format(
+        hours=hours, minutes=minutes, colon=colon
+    )
+    bbx, bby, bbwidth, bbh = clock_label.bounding_box
+    # Center the label
+    clock_label.x = round(display.width / 2 - bbwidth / 2)
+    clock_label.y = display.height // 2
+    if DEBUG:
+        print("Label bounding box: {},{},{},{}".format(bbx, bby, bbwidth, bbh))
+        print("Label x: {} y: {}".format(clock_label.x, clock_label.y))
+
+
+last_check = None
+update_time(show_colon=True)  # Display whatever time is on the board
+group.append(clock_label)  # add the clock label to the group
 
 while True:
-    # run 2*n generations.
-    # For the Conway tribute on 64x32, 80 frames is appropriate.  For random
-    # values, 400 frames seems like a good number.  Working in this way, with
-    # two bitmaps, reduces copying data and makes the animation a bit faster
-    for _ in range(n):
-        display.root_group = g1
-        apply_life_rule(b1, b2)
-        display.root_group = g2
-        apply_life_rule(b2, b1)
+    if last_check is None or time.monotonic() > last_check + 3600:
+        try:
+            update_time(
+                show_colon=True
+            )  # Make sure a colon is displayed while updating
+            # network.get_local_time()  # Synchronize Board's clock to Internet
+            last_check = time.monotonic()
+        except RuntimeError as e:
+            print("Some error occured, retrying! -", e)
 
-    # After 2*n generations, fill the board with random values and
-    # start over with a new color.
-    randomize(b1)
-    # Pick a random color out of 6 primary colors or white.
-    palette[1] = (
-        (0x0000ff if random.random() > .33 else 0) |
-        (0x00ff00 if random.random() > .33 else 0) |
-        (0xff0000 if random.random() > .33 else 0)) or 0xffffff
-    n = 200
+    update_time()
+    time.sleep(1)
